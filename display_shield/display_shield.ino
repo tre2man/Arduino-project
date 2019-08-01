f46d3b36d1e50cb52f10681a209571b5d8c2651d
tre2man <kimtree97@naver.com>
2019-08-01 20:43:28 +0900

    주석 달기
    주석 추가 및 코드 정리

--- display_shield/display_shield.ino
+++ display_shield/display_shield.ino
@@ -8,8 +8,6 @@ const int roll=A1; //가변저항 변수
 int lcd_key     = 0; //버튼을 읽어온 값 변환 후 저장하는 변수
 int adc_key_in  = 0; //버튼을 읽어온 값 저장하는 변수
 int now=1; //1 펄스 2 사인 3 트라이앵글
-int mode=0;
-int read_lcd=0;
  
 #define btnRIGHT  1
 #define btnUP     2
@@ -18,14 +16,11 @@ int read_lcd=0;
 #define btnSELECT 5
 #define btnNONE   6
  
-int read_LCD_buttons()
+int read_LCD_buttons()  //버튼 입력받는 함수 
 {               
-  //while(read_lcd==0)
-  //{
     adc_key_in = analogRead(0);
     if(adc_key_in<1000)
     {
-      read_lcd=1;
       if (adc_key_in < 50)   return btnRIGHT;  
       else if (adc_key_in < 195)  return btnUP; 
       else if (adc_key_in < 380)  return btnDOWN; 
@@ -34,23 +29,74 @@ int read_LCD_buttons()
     }
     else
     {
-      Serial.print("not input");
-      Serial.print(now);
-      Serial.print("\n");
-      return 7;
+      //Serial.print("not input");
+      //Serial.print(now);
+      //Serial.print("\n");
+      return 7;  //값이 입력되지 않았을 경우 상관없는 값 반환
     }
-  //}
-  //read_lcd=0;
-  delay(300);
-    // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
-    // we add approx 50 to those values and check to see if we are close  
 }
 
-float mainmenu()
+float PULSE()  //펄스파 
 {
-   lcd.setCursor(0,0);             // set the LCD cursor   position 
-   lcd.print("select-> ");  // print a simple message on the LCD
+  while(1)
+  {
+   lcd.setCursor(0,0); 
+   lcd.print("Mode is PULSE   ");
    lcd.setCursor(0,1);
+   lcd.print("Input Hz"); 
+
+   roll_();
+    
+   lcd_key = read_LCD_buttons(); 
+   if(lcd_key==btnLEFT) break;
+  }
+}
+
+float SINE()  //사인파 
+{
+  while(1)
+  {
+   lcd.setCursor(0,0); 
+   lcd.print("Mode is SINE   ");
+   lcd.setCursor(0,1);
+   lcd.print("Input Hz"); 
+
+   roll_();
+    
+   lcd_key = read_LCD_buttons(); 
+   if(lcd_key==btnLEFT) break;
+  }
+}
+
+float TRI()  //삼각파 
+{
+ while(1)
+ {
+   lcd.setCursor(0,0); 
+   lcd.print("Mode is TRI   ");
+   lcd.setCursor(0,1);
+   lcd.print("Input Hz"); 
+
+   roll_();
+    
+   lcd_key = read_LCD_buttons(); 
+   if(lcd_key==btnLEFT) break;
+ }
+}
+
+float roll_() //가변 저항의 저항값 읽어온 후 0~100까지의 수로 반환하는 함수 
+{
+   float button=analogRead(roll);
+   lcd.setCursor(10,1);
+   lcd.print(button/1023*100,0);
+   lcd.print("  "); 
+}
+
+
+float mainmenu() //메인 메뉴 함수 
+{
+   lcd.setCursor(0,0);            
+   lcd.print("select-> ");  //메인 메뉴 화면 구성하기 
    lcd.print("                ");
    lcd.setCursor(9,0);
 
@@ -69,80 +115,22 @@ float mainmenu()
   if(now==2)lcd.print("2.SINE ");
   if(now==3)lcd.print("3.TRI  ");
 
-  delay(200);
-}
-
-
-float PULSE()
-{
-  while(1)
-  {
-   lcd.setCursor(0,0); 
-   lcd.print("Mode is PULSE   ");
-   lcd.setCursor(0,1);
-   lcd.print("Input Hz"); 
-
-   roll_();
-    
-   lcd_key = read_LCD_buttons(); 
-   if(lcd_key==btnLEFT) break;
-  }
-}
-
-float SINE()
-{
-  while(1)
-  {
-   lcd.setCursor(0,0); 
-   lcd.print("Mode is SINE   ");
-   lcd.setCursor(0,1);
-   lcd.print("Input Hz"); 
-
-   roll_();
-    
-   lcd_key = read_LCD_buttons(); 
-   if(lcd_key==btnLEFT) break;
-  }
-}
-
-float TRI()
-{
- while(1)
- {
-   lcd.setCursor(0,0); 
-   lcd.print("Mode is TRI   ");
-   lcd.setCursor(0,1);
-   lcd.print("Input Hz"); 
-
-   roll_();
-    
-   lcd_key = read_LCD_buttons(); 
-   if(lcd_key==btnLEFT) break;
- }
-}
-
-float roll_()
-{
-   float button=analogRead(roll);
-   lcd.setCursor(10,1);
-   lcd.print(button/1023*100,0);
-   lcd.print("  "); 
+  delay(100);
 }
 
  
 void setup()
 {
-   lcd.begin(16, 2);               // start the library
-   lcd.setCursor(0,0);             // set the LCD cursor   position 
-   lcd.print("select-> 1.PULSE");  // print a simple message on the LCD
+   lcd.begin(16, 2);               // lcd 라이브러리 시작 
+   lcd.setCursor(0,0);             // 초기화면 세팅 
+   lcd.print("select-> 1.PULSE");  
    
-   Serial.begin(9600);
-   pinMode(A1,INPUT);
+   //Serial.begin(9600);
+   pinMode(A1,INPUT);  //가변저항 값 입력받기 위한 세팅 
 }
  
 void loop()
-{ 
-   //lcd.setCursor(9,0);             // move to the begining of the second line
-   Serial.print("new loop\n");   
+{
+   //Serial.print("new loop\n");   
    mainmenu();
 }
