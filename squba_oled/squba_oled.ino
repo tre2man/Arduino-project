4654122cc317247ecdb0abc00214f4c7dfd996a9
tre2man <kimtree97@naver.com>
2019-08-20 20:52:47 +0900

    1
    1

--- squba_oled/squba_oled.ino
+++ squba_oled/squba_oled.ino
@@ -15,9 +15,9 @@ void loop()
   if(cds>100) cds=100;
   u8g.firstPage();
   do{
-      u8g.drawBox(1,1,cds/100*126,10);
+      u8g.drawBox(1,1,cds/100*126,10); //좌측 상단의 좌표는 1,1 ,가로길이는 cds/100*126, 세로 길이는 10
       u8g.setFont(u8g_font_unifont);
       u8g.setPrintPos(0,30);
-      u8g.print(cds);
+      u8g.print(cds,0);
   }while(u8g.nextPage());
 }
