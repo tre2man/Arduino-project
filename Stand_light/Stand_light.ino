25240b4415461008015e34ee289fa40097d29a3f
tre2man <kimtree97@naver.com>
2020-04-19 01:41:07 +0900

    Update Stand_light.ino

--- Stand_light/Stand_light.ino
+++ Stand_light/Stand_light.ino
@@ -2,8 +2,8 @@
 #define input A0; //입력저항 포트
 
 #define Pin 5; //네오픽셀 출력 핀번호
-#define NUM_LEDS 6; //led 개수
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+#define NUM_LEDS 30; //led 개수
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
 
 void setup() {
   pinMode(input,INPUT);
@@ -14,8 +14,9 @@ void setup() {
 void loop() {
   int state = analogRead(input);
   state = map(state,0,1023,0,255);
-  for(uint16_t i=0;i<NUM_LEDS;i++){
-    strip.setPixelColor(i,strip.Color(127, 127, 127));
+  for(int i=0;i<NUM_LEDS;i++){
+    strip.setPixelColor(i,strip.Color(0,0,0,255));
     strip.show();
+    strip.setBrightness(state);
   }
 }
