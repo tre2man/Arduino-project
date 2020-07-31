15246751df6ff00967d59262d153fed79a729f5a
tre2man <45779423+tre2man@users.noreply.github.com>
2020-07-31 09:34:31 +0900

    캡스톤
    캡스톤

--- Liquidcrysal_i2c/Liquidcrysal_i2c.ino
+++ Liquidcrysal_i2c/Liquidcrysal_i2c.ino
@@ -2,23 +2,30 @@
 
 #include <Wire.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+int i = 0;
+
+LiquidCrystal_I2C lcd1(0x27, 16, 2);
+LiquidCrystal_I2C lcd2(0x26, 16, 2);
 
 void setup() {
-
-Serial.begin(115200);
-
-//Use predefined PINS consts
-
-Wire.begin(D2, D1);
-
-lcd.begin();
-
-lcd.home();
-
-lcd.print("Hello, NodeMCU");
-
+  Serial.begin(115200);
+  Wire.begin(D2, D1);
+  lcd1.begin();
+  lcd2.begin();
 }
 
 void loop() { // do nothing here 
-  }
+  
+  lcd1.begin();
+  lcd1.home();
+  lcd1.print("Namwoo Kim");
+  lcd1.setCursor(0,1);
+  lcd1.print("Heejin Kim");
+
+  lcd2.begin();
+  lcd2.home();
+  lcd2.print("Dongseob Sin");
+  lcd2.setCursor(0,1);
+  lcd2.print("Hyonsik Kim");
+ 
+}
--- phpUpload/phpUpload.ino
+++ phpUpload/phpUpload.ino
@@ -6,16 +6,16 @@ const char* ssid     = "ppp";
 const char* password = "0000003940";
 String SERVER = "192.168.43.87";
 String host = "/insert_data.php?";
-//String host = "/insert_data.php?temp=10";
 
 //와이파이 서버 설정
 WiFiServer server(80);
 WiFiClient client;
 HTTPClient http;
 
-//~초마다 실행하기 위한 변수
+//~초마다 실행하기 위한 변수 시간 주의
 unsigned long previousMillis = 0;
-const long interval = 10000; 
+const long interval = 1000;
+//const long interval = 3600000;
 
 //각 칸에 맞는 변수만 골라서 사용함.
 static int Analog = A0; 
@@ -44,6 +44,8 @@ String date,times;
 int timezone = 3; 
 int dst = 0; 
 
+int faketime = 0;
+
 void setup() {
   Serial.begin(115200);
 
@@ -62,11 +64,9 @@ void setup() {
   pinMode(Digital2,INPUT);
   pinMode(Digital3,INPUT);
   pinMode(Digital4,INPUT);
-
-  //모터드라이버는 3개의 핀 사용함
-  pinMode(Digital5,OUTPUT);
-  pinMode(Digital6,OUTPUT);
-  pinMode(Digital7,OUTPUT);
+  pinMode(Digital5,INPUT);
+  pinMode(Digital6,INPUT);
+  //pinMode(Digital7,INPUT);
 
   //시간 알기 위한 소스
   configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov"); 
@@ -90,41 +90,30 @@ void loop() {
     digitalData2 = digitalRead(D2);
     digitalData3 = digitalRead(D3);
     digitalData4 = digitalRead(D4);
-    
-    /*
     digitalData5 = digitalRead(D5);
     digitalData6 = digitalRead(D6);
-    digitalData7 = digitalRead(D7);
-    */
+    //digitalData7 = digitalRead(D7);
+
+    Serial.print("LivingRoom Status : ");
+    Serial.println(digitalData0);
+    Serial.print("Room1 Status : ");
+    Serial.println(digitalData1);
+    Serial.print("Room2 Status : ");
+    Serial.println(digitalData2);ㄴ
+    Serial.print("Bathroom Status : ");
+    Serial.println(digitalData3);
+    Serial.print("Toilet Status : ");
+    Serial.println(digitalData4);
+    Serial.print("Water Status : ");
+    Serial.println(digitalData5);
+    Serial.print("PIR Status : ");
+    Serial.println(digitalData6);
+    Serial.print("Gas Status : ");
+    Serial.println(AnalogData);
 
     time_t now = time(nullptr); 
     struct tm * timeinfo;
     timeinfo = localtime(&now);
-
-    //수류모터 동작 코드
-    /*
-     * if(동작조건) sink(0)
-     * if(멈춤조건) sink(1)
-     */
-    
-    /*
-    String Syear = String((timeinfo->tm_year)+1900, DEC);
-    String Smonth = String((timeinfo->tm_mon)+1, DEC);
-    String Sday = String((timeinfo->tm_mday), DEC);
-    String Shour = String((timeinfo->tm_hour), DEC);
-    String Smin = String((timeinfo->tm_min), DEC);
-    String Ssec = String((timeinfo->tm_sec), DEC);
-
-    if((timeinfo->tm_mon)+1<10) Smonth =  "0" + String((timeinfo->tm_mon)+1);
-    if((timeinfo->tm_mday)<10) Sday =  "0" + String(timeinfo->tm_mday);
-    if((timeinfo->tm_hour)<10) Shour =  "0" + String(timeinfo->tm_hour);
-    if((timeinfo->tm_min)<10) Smin =  "0" + String(timeinfo->tm_min);
-    if((timeinfo->tm_sec)<10) Ssec =  "0" + String(timeinfo->tm_sec);
-   
-    
-    date = Syear + "-" + Smonth + "-" + Sday; 
-    times = Shour + ":" + Smin + ":" + Ssec;
-    */
 
     String Syear = String((timeinfo->tm_year)-100, DEC);
     String Smonth = String((timeinfo->tm_mon)+1, DEC);
@@ -139,8 +128,9 @@ void loop() {
     if((timeinfo->tm_min)<10) Smin =  "0" + String(timeinfo->tm_min);
     if((timeinfo->tm_sec)<10) Ssec =  "0" + String(timeinfo->tm_sec);
 
-    date = Syear + Smonth + Sday; 
-    times = Shour;
+    date = Syear + "-" + Smonth + "-" + Sday; 
+    //times = Shour;
+    times = Shour + Smin;
   
     upload();
   }
@@ -153,31 +143,12 @@ void upload() {
 
   if (client.connect(SERVER, 80))
   {
-    /*
     client.print(String("GET ") + 
-    host + "temp=" + String(AnalogData) + "&sw1=" + String(digitalData0) + "&sw2=" + String(digitalData1) + "&date=" + String(date) + "&time=" + String(times) +
-    " HTTPs/1.1\r\n" + "Host: " + SERVER + "\r\n" + "Connection: close\r\n\r\n");
-    */
-
-    client.print(String("GET ") + 
-    host + "temp=" + String(AnalogData) + "&sw1=" + String(digitalData0) + "&sw2=" + String(digitalData1) + "&date=" + String(date) + "&time=" + String(times) +
+    host + "LivingRoom=" + String(digitalData0) + "&Room1=" + String(digitalData1) + "&Room2=" + String(digitalData2) + "&Bathroom=" + String(digitalData3) + "&Toilet=" + String(digitalData4) + "&Water=" + String(digitalData5) + "&PIR=" + String(digitalData1)  + "&Gas=" + String(digitalData1) + "&date=" + String(date) + "&time=" + String(times) +
     " HTTP/1.1\r\n" + "Host: " + SERVER + "\r\n" + "Connection: close\r\n\r\n");
 
     Serial.println(String("GET ") + 
-    host + "temp=" + String(AnalogData) + "&sw1=" + String(digitalData0) + "&sw2=" + String(digitalData1) + "&date=" + String(date) + "&time=" + String(times) +
+   host + "LivingRoom=" + String(digitalData0) + "&Room1=" + String(digitalData1) + "&Room2=" + String(digitalData2) + "&Bathroom=" + String(digitalData3) + "&Toilet=" + String(digitalData4) + "&Water=" + String(digitalData5) + "&PIR=" + String(digitalData1)  + "&Gas=" + String(digitalData1) + "&date=" + String(date) + "&time=" + String(times) +
     " HTTP/1.1\r\n" + "Host: " + SERVER + "\r\n" + "Connection: close\r\n\r\n");
-  }
-}
-
-void sink(state){
-  if(state){
-    digitalWrite(Digital7,HIGH);
-    digitalWrite(Digital8,LOW);
-    digitalWrite(Digital9,100);
-  }
-  else{
-    digitalWrite(Digital7,LOW);
-    digitalWrite(Digital8,HIGH);
-    digitalWrite(Digital9,0);
   }
 }
